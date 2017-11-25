db881995b509a3724af450f8e18e4e75309bd282
Anggi Riyandi <anggi.riyandi@gmail.com>
2017-11-25 14:25:06 +0700

    first commit

---
+++ src/main/java/com/belajar/belajarspringboot/contoller/HalloController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.belajar.belajarspringboot.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ *
+ * @author anggi
+ */
+
+@RestController
+public class HalloController {
+    
+    @GetMapping("/hallo")
+    public String halloWorld(){
+        return "hallo anggi";
+    }
+    
+}
